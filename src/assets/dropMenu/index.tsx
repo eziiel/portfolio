64e1a43723d5c5561ad972c81025cce3f6961fc5
eziel <eziielp10@hotmail.com>
2022-09-14 10:04:21 -0300

    navDropMenu

---
+++ src/assets/dropMenu/index.tsx
@@ -0,0 +1,48 @@
+import React from "react"
+import * as S from "./styled"
+import drop from "../datas/dropMenu.json"
+import { MM } from "../textCode/mm"
+import { ContextRef } from "../../context"
+
+export const DropMenu: React.FC = () => {
+  const [active, setActive] = React.useState(false)
+  // const [iconActive, SetIconActive] = React.useState(0)
+  const { component, setComponent } = React.useContext(ContextRef)
+
+  const handleDropMenu = (): void => {
+    setActive(!active)
+  }
+
+  const handleIconActive = (id: number): void => {
+    setComponent(id)
+  }
+
+  return (
+    <S.Header onClick={handleDropMenu}>
+      <S.ImgContent>
+        <S.ImgActive src={drop[component].src} alt={drop[component].src} />
+      </S.ImgContent>
+      {active && (
+        <S.Nav>
+          <S.Ul>
+            {drop.map(({ id, src, text, alt }) => (
+              <S.Li key={id}>
+                <S.LinkIcon
+                  href={`#${text}`}
+                  onClick={() => handleIconActive(id)}
+                >
+                  <S.ImgIcon src={src} alt={alt} />
+                  {active && (
+                    <S.NameIcon>
+                      <MM text={text} />
+                    </S.NameIcon>
+                  )}
+                </S.LinkIcon>
+              </S.Li>
+            ))}
+          </S.Ul>
+        </S.Nav>
+      )}
+    </S.Header>
+  )
+}
--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -4,6 +4,7 @@ import Images from "../../assets/datas/images.json"
 import Redes from "../../assets/datas/imagesRedes.json"
 import { MM } from "../../assets/textCode/mm"
 import { ContextRef } from "../../context"
+import { DropMenu } from "../../assets/dropMenu"
 
 export const NavBar: React.FC = () => {
   const { component, setComponent } = React.useContext(ContextRef)
@@ -14,31 +15,34 @@ export const NavBar: React.FC = () => {
   }
 
   return (
-    <S.Header id="Nav" ref={head}>
-      <S.Nav>
-        <S.Ul>
-          {Images.map(({ src, alt, text, id }) => (
-            <S.Li key={alt} onClick={() => handleStatusMenu(id)}>
-              <S.LinkForPage href={`#${text}`}>
-                <S.Img src={src} alt={alt} status={id === component}></S.Img>
-                <S.SpanMenu status={id === component}>
+    <>
+      <S.Header id="Nav" ref={head}>
+        <S.Nav>
+          <S.Ul>
+            {Images.map(({ src, alt, text, id }) => (
+              <S.Li key={alt} onClick={() => handleStatusMenu(id)}>
+                <S.LinkForPage href={`#${text}`}>
+                  <S.Img src={src} alt={alt} status={id === component}></S.Img>
+                  <S.SpanMenu status={id === component}>
+                    <MM text={text} />
+                  </S.SpanMenu>
+                </S.LinkForPage>
+              </S.Li>
+            ))}
+          </S.Ul>
+          <S.NavRedes>
+            {Redes.map(({ id, src, alt, link, text }) => (
+              <li key={id}>
+                <S.LinkForPage href={link} target="_blank">
+                  <S.Img src={src} alt={alt} />
                   <MM text={text} />
-                </S.SpanMenu>
-              </S.LinkForPage>
-            </S.Li>
-          ))}
-        </S.Ul>
-        <S.NavRedes>
-          {Redes.map(({ id, src, alt, link, text }) => (
-            <li key={id}>
-              <S.LinkForPage href={link} target="_blank">
-                <S.Img src={src} alt={alt} />
-                <MM text={text} />
-              </S.LinkForPage>
-            </li>
-          ))}
-        </S.NavRedes>
-      </S.Nav>
-    </S.Header>
+                </S.LinkForPage>
+              </li>
+            ))}
+          </S.NavRedes>
+        </S.Nav>
+      </S.Header>
+      <DropMenu />
+    </>
   )
 }
