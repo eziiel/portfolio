e1e2bd3dc7fb0ce55f7a9f2910736ed1bdf2b136
eziiel <eziielp10@hotmail.com>
2023-07-02 03:27:47 -0300

    refactor: scrol smooth

--- src/pages/experiencia/index.tsx
+++ src/pages/experiencia/index.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import * as S from './styled'
 import { Title } from '../../styleds/forComponents/geral'
-import ferramentas from '../../api/ferramentas.json'
+// import ferramentas from '../../api/ferramentas.json'
 import { MM } from '../../assets/textCode/mm'
 import { Observe } from '../../assets/observer'
 
 export const Experiencia: React.FC = () => {
+  const [ferramentas, setFerramentas] = React.useState([])
+
+  React.useEffect(() => {
+    fetch('../../api/ferramentas.json')
+      .then((response) => response.json())
+      .then((res) => setFerramentas(res))
+  }, [])
+
   return (
     <S.ExperienciaSection id="Experiencia">
       <Observe reference="#Experiencia" />
@@ -14,7 +22,7 @@ export const Experiencia: React.FC = () => {
       </Title>
       <S.ExperienciaInfo>
         <S.Ferramentas>
-          {ferramentas.map(({ id, src, alt, text }) => (
+          {ferramentas?.map(({ id, src, alt, text }) => (
             <S.IconsFerramentas key={id}>
               <S.Img src={src} alt={alt} />
               <S.NameIcon>{text}</S.NameIcon>
